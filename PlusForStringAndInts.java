69749f08e2cb855be92f3f7d525e0a7b33051297
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-20 10:34:44 +0300

    Add files via upload

---
+++ PlusForStringAndInts.java
@@ -0,0 +1,15 @@
+package javatraining.oletsky.javastrings;
+
+/**
+ * A canonical illustration of using + for String and int
+ */
+
+public class PlusForStringAndInts {
+    public static void main(String[] args) {
+        int a=5;
+        int b=3;
+        String s="|output|";
+        System.out.println (s+a+b);
+        System.out.println (a+b+s);
+    }
+}
